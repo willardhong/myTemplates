8165c39390f9b4b9600f39c2de8661874ee2848d
willardhong <willard8hong@gmail.com>
2020-03-12 20:46:52 -0700

    second commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 # nobel_viz.py
 
 from flask import Flask
+import pandas as pd
+
+df_winners = pd.read_json('data/nobel_winners.json')
+
+for name, group in df_winners.groupby('country'): 1
+    group.to_json('data/winners_by_country' + name + '.json',\
+                  orient='records')
+
 app = Flask(__name__)
 
 @app.route("/")  
